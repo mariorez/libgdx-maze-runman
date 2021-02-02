d57c44419c7083aa429e6603f5b3e3f32280b9d7
Mario Rezende <mariorez@gmail.com>
2021-02-02 19:32:32 -0300

    adds coins into maze and win/lost rules

---
+++ core/src/main/java/org/seariver/actor/Coin.java
@@ -0,0 +1,13 @@
+package org.seariver.actor;
+
+import com.badlogic.gdx.scenes.scene2d.Stage;
+import org.seariver.BaseActor;
+
+public class Coin extends BaseActor {
+
+    public Coin(float x, float y, Stage stage) {
+        super(x, y, stage);
+        loadTexture("coin.png");
+        setBoundaryPolygon(6);
+    }
+}
--- core/src/main/java/org/seariver/screen/LevelScreen.java
+++ core/src/main/java/org/seariver/screen/LevelScreen.java
@@ -2,9 +2,12 @@ package org.seariver.screen;
 
 import com.badlogic.gdx.Input;
 import com.badlogic.gdx.graphics.Color;
+import com.badlogic.gdx.scenes.scene2d.actions.Actions;
+import com.badlogic.gdx.scenes.scene2d.ui.Label;
 import org.seariver.BaseActor;
 import org.seariver.BaseGame;
 import org.seariver.BaseScreen;
+import org.seariver.actor.Coin;
 import org.seariver.actor.Ghost;
 import org.seariver.actor.Hero;
 
@@ -13,6 +16,9 @@ public class LevelScreen extends BaseScreen {
     Maze maze;
     Hero hero;
     Ghost ghost;
+
+    Label coinsLabel;
+    Label messageLabel;
 
     public void initialize() {
 
@@ -28,12 +34,60 @@ public class LevelScreen extends BaseScreen {
 
         ghost = new Ghost(0, 0, mainStage);
         ghost.centerAtActor(maze.getRoom(11, 9));
+
+        for (BaseActor room : BaseActor.getList(mainStage, "org.seariver.actor.Room")) {
+            Coin coin = new Coin(0, 0, mainStage);
+            coin.centerAtActor(room);
+        }
+
+        ghost.toFront();
+
+        coinsLabel = new Label("Coins left:", BaseGame.labelStyle);
+        coinsLabel.setColor(Color.GOLD);
+        messageLabel = new Label("...", BaseGame.labelStyle);
+        messageLabel.setFontScale(2);
+        messageLabel.setVisible(false);
+
+        uiTable.pad(10);
+        uiTable.add(coinsLabel);
+        uiTable.row();
+        uiTable.add(messageLabel).expandY();
     }
 
     // Game Loop
     public void update(float deltaTime) {
+
         for (BaseActor wall : BaseActor.getList(mainStage, "org.seariver.actor.Wall")) {
             hero.preventOverlap(wall);
+        }
+
+        for (BaseActor coin : BaseActor.getList(mainStage, "org.seariver.actor.Coin")) {
+            if (hero.overlaps(coin)) {
+                coin.remove();
+            }
+        }
+
+        int coins = BaseActor.count(mainStage, "org.seariver.actor.Coin");
+        coinsLabel.setText("Coins left: " + coins);
+
+        if (coins == 0) {
+            ghost.remove();
+            ghost.setPosition(-1000, -1000);
+            ghost.clearActions();
+            ghost.addAction(Actions.forever(Actions.delay(1)));
+            messageLabel.setText("You win!");
+            messageLabel.setColor(Color.GREEN);
+            messageLabel.setVisible(true);
+        }
+
+        if (hero.overlaps(ghost)) {
+            hero.remove();
+            hero.setPosition(-1000, -1000);
+            ghost.clearActions();
+            ghost.addAction(Actions.forever(Actions.delay(1)));
+            messageLabel.setText("Game Over");
+            messageLabel.setColor(Color.RED);
+            messageLabel.setVisible(true);
         }
 
         if (ghost.getActions().size == 0) {
